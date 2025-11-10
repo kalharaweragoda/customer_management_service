f11f9371942a1648f1042c83d10b09743fc54d07
kalharaweragoda <kalharaweragoda@gmail.com>
2025-11-10 14:41:42 +0530

    dto file add

--- src/main/java/edu/icet/controller/CustomerController.java
+++ src/main/java/edu/icet/controller/CustomerController.java
@@ -1,4 +1,13 @@
 package edu.icet.controller;
 
+import edu.icet.dto.Customer;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CustomerController {
+
+    public void addCustomer(Customer customer){
+
+    }
+
 }
