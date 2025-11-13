34310448ad5499b70567abfdbdbff63f71b9c2fb
Kalhara Weragoda <kalharaweragoda@gmail.com>
2025-11-13 23:37:37 +0530

    Add files via upload
    index.html add

---
+++ Customer-Manage-Front-End/app.js
@@ -0,0 +1,33 @@
+loadCustomer();
+function loadCustomer() {
+  fetch("http://localhost:8080/customer/get-all")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+
+      let tableRow = `
+        <caption>2025 Customers</caption>
+        <tr>
+            <td>ID</td>
+            <td>NAME</td>
+            <td>ADDRESS</td>
+            <td>SALARY</td>
+        </tr>
+        `;
+
+      let customerTable = document.getElementById("tblCustomers");
+
+      data.forEach(customer => {
+        tableRow += `        
+        <tr>
+            <td>${customer.id}</td>
+            <td>${customer.name}</td>
+            <td>${customer.address}</td>
+            <td>${customer.salary}</td>
+        </tr>
+            `;
+      });
+
+      customerTable.innerHTML = tableRow;
+    });
+}
